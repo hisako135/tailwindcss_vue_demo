8bc1bf56b692a6235c2180811cd665600ccfb294
Hisako135 <baiqi_shouzi@hotmail.com>
2019-12-22 05:02:21 +0900

    Tweaked

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,22 @@
 // postcss.config.js
-const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 const purgecss = require('@fullhuman/postcss-purgecss')({
-
   // Specify the paths to all of the template files in your project 
   content: [
     './src/**/*.vue',
   ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  defaultExtractor: (content) => {
+    const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+    return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+  },
+  whitelistPatterns: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
 })
 
 module.exports = {
   plugins: [
-    autoprefixer,
     tailwindcss,
+    autoprefixer,
     ...process.env.NODE_ENV === 'production'
     ? [purgecss]
     : []
